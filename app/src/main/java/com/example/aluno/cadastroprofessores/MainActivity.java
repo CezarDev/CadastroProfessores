9532f83288beb212658123b091126255b94b5471
CezarDev <cezar.alves.dev@gmail.com>
2019-06-24 11:14:58 -0400

    Adicionando os metodos

--- app/src/main/java/com/example/aluno/cadastroprofessores/MainActivity.java
+++ app/src/main/java/com/example/aluno/cadastroprofessores/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.aluno.cadastroprofessores;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
     EditText edNome, edDisciplina, edEmail;
     ListView listView;
+    Professor professorSelecionado;
 
     FirebaseDatabase firebaseDatabase;
     DatabaseReference databaseReference;
@@ -33,13 +40,52 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        edNome       = findViewById(R.id.editTextProfessorId);
+        edNome = findViewById(R.id.editTextProfessorId);
         edDisciplina = findViewById(R.id.editTextDisciplinaId);
-        edEmail      = findViewById(R.id.editTextEmailId);
-        listView     = findViewById(R.id.listViewId);
+        edEmail = findViewById(R.id.editTextEmailId);
+        listView = findViewById(R.id.listViewId);
 
 
-            iniciarFirebase();
+        iniciarFirebase();
+
+        eventoFirebase();
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long l) {
+
+                professorSelecionado = (Professor) parent.getItemAtPosition(position);
+                edNome.setText(professorSelecionado.getNome());
+                edDisciplina.setText(professorSelecionado.getDisciplina());
+                edEmail.setText(professorSelecionado.getEmail());
+            }
+        });
+
+
+    }
+
+    private void eventoFirebase() {
+        databaseReference.child("Professor").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                professorList.clear();
+
+                for (DataSnapshot obDataSnapshot1 : dataSnapshot.getChildren()) {
+                    Professor p = obDataSnapshot1.getValue(Professor.class);
+                    professorList.add(p);
+                }
+
+                professorArrayAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, professorList);
+
+                listView.setAdapter(professorArrayAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     @Override
@@ -48,7 +94,7 @@ public class MainActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
-    private void iniciarFirebase(){
+    private void iniciarFirebase() {
         FirebaseApp.initializeApp(MainActivity.this);
         firebaseDatabase = FirebaseDatabase.getInstance();
         databaseReference = firebaseDatabase.getReference();
@@ -59,7 +105,7 @@ public class MainActivity extends AppCompatActivity {
 
         int id = item.getItemId();
 
-        if(id== R.id.menu_novo){
+        if (id == R.id.menu_novo) {
 
             Professor p = new Professor();
             p.setNome(edNome.getText().toString());
@@ -68,19 +114,26 @@ public class MainActivity extends AppCompatActivity {
             p.setId(UUID.randomUUID().toString());
 
             databaseReference.child("Professor").child(p.getId()).setValue(p);
+
+            limparCampos();
+        }
+            if (id == R.id.menu_atualizar) {
+
+            }
+
+            if (id == R.id.menu_deletar) {
+
+            }
+
+
+            return super.onOptionsItemSelected(item);
         }
 
-        if(id== R.id.menu_atualizar){
-
+        private void limparCampos(){
+                edNome.setText("");
+                edDisciplina.setText("");
+                edEmail.setText("");
         }
 
-        if(id== R.id.menu_deletar){
-
-        }
-
-
-
-
-        return super.onOptionsItemSelected(item);
-    }
 }
+
--- app/src/main/java/com/example/aluno/cadastroprofessores/Professor.java
+++ app/src/main/java/com/example/aluno/cadastroprofessores/Professor.java
@@ -40,4 +40,9 @@ public class Professor {
     public void setEmail(String email) {
         this.email = email;
     }
+
+
+    @Override
+    public String toString() {
+        return "Professor: " + nome + "Disciplina: " + disciplina;    }
 }
